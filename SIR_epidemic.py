650826047e47bc21e704c54956f9f84e821aaf31
Brandon Finley <brfi3983@colorado.edu>
2020-12-02 16:24:20 -0700

    made it into a class so we can import into our main driver

--- SIR_epidemic.py
+++ SIR_epidemic.py
@@ -3,40 +3,45 @@ import numpy.random as rand
 
 # A is the adjacency matrix as a numpy array.
 # Vaccination not yet implemented
+class SIR_class():
+    def __init__(self, A)
+        self.A = A
+        self.n = A.shape[0]
 
-def begin_epidemic(A, vaccinate = False):
-    n = A.shape[0]
-    #find v_0
-    return rand.randint(0, n-1)
+    def begin_epidemic(vaccinate = False):
 
+        #find v_0
+        return rand.randint(0, self.n-1)
 
-def SIR(A, beta, mu, delta_t, T, v_0, vaccinated = False):
-    # Initialize variables
-    num_timesteps = T/delta_t
-    q = mu * delta_t
-    p = beta * delta_t
+    def SIR(beta, mu, delta_t, T, v_0, vaccinated = False):
+        # Initialize variables
+        num_timesteps = T/delta_t
+        q = mu * delta_t
+        p = beta * delta_t
 
-    susceptible_nodes = np.r_[0:(A.shape[0] - 1):1] # all nodes susceptible at first
-    susceptible_nodes.remove(v_0)   # remove first infected
+        susceptible_nodes = np.r_[0:(self.A.shape[0] - 1):1] # all nodes susceptible at first
+        susceptible_nodes.remove(v_0)   # remove first infected
 
-    infectious_nodes = [v_0]
-    recovered_nodes = np.empty(0)
+        infectious_nodes = [v_0]
+        recovered_nodes = np.empty(0)
 
-    # Main loop
-    for t in (1,num_timesteps):
-        for v in infectious_nodes:
-            # Neighbors of v become infected with probability q
-            for s in susceptible_nodes:
-                if A[v,s].equals(1):
-                    r = rand.uniform(0,1)
-                    if r > p:
-                        infectious_nodes.append(s)
-                        susceptible_nodes.remove(s)
-            r = rand.uniform(0,1)
-            if r > q:
-                recovered_nodes.append(v)
-                infectious_nodes.remove(v)
-    
+        # Main loop
+        for t in (1,num_timesteps):
+            for v in infectious_nodes:
+                # Neighbors of v become infected with probability p
+                for s in susceptible_nodes:
+                    if self.A[v,s].equals(1):
+                        r = rand.uniform(0,1)
+                        if r > p:
+                            infectious_nodes.append(s)
+                            susceptible_nodes.remove(s)
+                r = rand.uniform(0,1)
+                if r > q:
+                    recovered_nodes.append(v)
+                    infectious_nodes.remove(v)
+
+        return [susceptible_nodes, infectious_nodes, recovered_nodes]
+
 
 
 
